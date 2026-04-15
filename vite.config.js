ec74f0226ac0c19bb7ac62eb2923ebd72e9a3dd7
Claus Höfele <claus@claushoefele.com>
2026-04-15 10:56:32 +0200

    Use a custom action to deploy the app

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/sd73-projectc5/',
   plugins: [react(), tailwindcss()],
   test: {
     environment: 'jsdom',
